553e5ff3c8029e6d708ed85746843907ae2a6814
Alkut Askar <elkut.esker@gmail.com>
2025-10-17 20:52:49 -0400

    second v of background picker

---
+++ background-picker-2/js/main.js
@@ -0,0 +1,25 @@
+document.getElementById('purple').onclick = SetPurple
+document.getElementById('green').onclick = SetGreen
+document.getElementById('blue').onclick = SetBlue
+document.getElementById('black').onclick = SetBlack
+
+function SetPurple() {
+    document.querySelector('body').style.backgroundColor = 'rgba(241, 63, 247, 1)'
+    document.querySelector('body').style.color = 'white'
+}
+
+function SetGreen() {
+    document.querySelector('body').style.backgroundColor = 'rgba(0, 253, 81, 1)'
+    document.querySelector('body').style.color = 'white'
+}
+
+function SetBlue() {
+    document.querySelector('body').style.backgroundColor = 'rgba(0, 254, 255)'
+    document.querySelector('body').style.color = 'white'
+}
+
+function SetBlack() {
+    document.querySelector('body').style.backgroundColor = 'rgb(0, 0, 0)'
+    document.querySelector('body').style.color = 'white'
+    document.querySelector('ul').style.borderColor = 'white'
+}
